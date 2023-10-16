870c4eedeb191ca88c73485e46bee4bf0932f1e2
Nícolas Kormann <nicolas.kormann@proton.me>
2023-10-16 14:24:09 -0300

    prev/next nav

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-import Slide from "./slide.js";
+import { SlideNav } from "./slide.js";
 
-const slide = new Slide(".slide", ".wrapper");
+const slide = new SlideNav(".slide", ".wrapper");
 slide.init();
 
-slide.changeSlide(2);
-slide.activePrevSlide();
+slide.addArrow(".prev", ".next");
